f22c3d7b7a9d1456463cb223fa6aff04450a7520
T00fy <mostafa.alsari@gmail.com>
2016-05-27 16:24:18 +1200

    began character select script

---
+++ Assets/Scripts/CharacterSelect.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterSelect : MonoBehaviour {
+    public GameObject cursor;
+
+    private GameObject selectedGameObject;
+    private Text selectedText;
+    private CursorMover cm;
+    private GameObject selectedCharacter;
+
+    void Start()
+    {
+        cm = cursor.GetComponent<CursorMover>();
+        selectedGameObject = cm.GetSelectedOption();
+        selectedCharacter = selectedGameObject;
+        selectedText = selectedGameObject.GetComponent<Text>();
+    }
+	// Update is called once per frame
+	void Update () {
+
+
+        selectedText.color = Color.red;
+        if (Input.GetButtonDown("Fire1"))
+        {
+            selectedGameObject = cm.GetSelectedOption();
+            if (selectedGameObject.transform.IsChildOf(transform))
+            {
+                selectedText.color = Color.white;
+                selectedText = selectedGameObject.GetComponent<Text>();
+                selectedCharacter = selectedGameObject;
+                selectedText.color = Color.red;
+            }
+
+        }
+
+    }
+
+    public GameObject GetSelectedCharacter()
+    {
+        return selectedCharacter;
+    }
+}
